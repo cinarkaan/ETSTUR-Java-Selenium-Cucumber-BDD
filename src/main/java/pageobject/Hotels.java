79ddbcef8c04c474e1e9913aa77452d7cda7994a
cinarkaan <cnrkaan98@gmail.com>
2022-02-27 19:09:52 +0300

    Update Hotels.java

--- src/main/java/pageobject/Hotels.java
+++ src/main/java/pageobject/Hotels.java
@@ -7,26 +7,29 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
 import java.util.Random;
 
 public class Hotels {
-
-    WebDriver webDriver;
+    
     Random random = new Random();
 
     @FindBy(how = How.ID, using = "selectedCountText")
     private WebElement count;
 
+    @FindBy(how = How.CLASS_NAME, using = "hotelCardItem")
+    private List<WebElement> hotels;
+
     public Hotels (WebDriver webDriver) {
-        this.webDriver = webDriver;
         PageFactory.initElements(webDriver,this);
     }
 
     public void selectHotel (){
-        int hotelCount = Integer.parseInt(count.getText());
+        int hotelCount = hotels.size();
         System.out.println("Hotel count : " + hotelCount);
         int selectedHotelNumber = random.nextInt(hotelCount - 1) + 1;
-        webDriver.findElement(By.xpath("//*[@id='hotelList']/div["+selectedHotelNumber+"]")).click();
+        System.out.println("Select Hotel Number : " + selectedHotelNumber);
+        hotels.get(selectedHotelNumber).findElement(By.xpath("//*[@id=\"hotelList\"]/div["+selectedHotelNumber+"]/a/div/div/div[2]/div/div[2]/div[3]/ul/li[3]/i")).click();
     }
 
 }
